3eb2a3ab7501c9fcf17b5de2c31f9fd2f29d1e5d
Severin <sevefriede@gmail.com>
2017-01-06 21:48:59 +0100

    doing some production optimization

---
+++ client/init.tsx
@@ -0,0 +1,26 @@
+let scripts = [ '/client/bundle.js' ];
+
+let newBrowser = (
+  'fetch' in window &&
+  'Promise' in window &&
+  'assign' in Object &&
+  'keys' in Object
+);
+
+if (!newBrowser) {
+  scripts.unshift('/client/polyfills.js');
+}
+
+scripts.forEach((src) => {
+  let scriptEl = document.createElement('script');
+  scriptEl.src = src;
+  scriptEl.async = false;
+  document.head.appendChild(scriptEl);
+});
+
+if (`serviceWorker` in navigator) {
+  navigator.serviceWorker.register('/sw.js', { scope: '/client' })
+    .catch((error) => console.warn('service worker registration failed with ' + error));
+} else {
+  console.warn('service worker API not available. poor you.');
+}
